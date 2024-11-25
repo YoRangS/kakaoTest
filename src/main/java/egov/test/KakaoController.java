0b2472aab097f5f4318eb035e7a69fa8916344ad
joon0017 <21900699@handong.ac.kr>
2024-11-25 10:18:00 +0900

    restapi connection test

--- src/main/java/egov/test/KakaoController.java
+++ src/main/java/egov/test/KakaoController.java
@@ -1,12 +1,26 @@
 package egov.test;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/test")
 public class KakaoController {
-	@RequestMapping(value = "/hello.do")
-	public String hello() {
-		return "hello";
+//	@RequestMapping(value = "/hello.do")
+//	public String hello() {
+//		return "hello";
+//	}
+
+	@GetMapping("/{name}")
+	public String testFunction(@PathVariable String name) {
+		String returnValue = "test return value RESTAPI: " + name;
+		
+		return returnValue;
 	}
 }
