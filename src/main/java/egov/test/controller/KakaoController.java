575dc0727c5a524b0cc02d5a413f04f4a7e97b44
YoRangS <21900673@handong.ac.kr>
2024-12-06 19:47:00 +0900

    Get 201

--- src/main/java/egov/test/controller/KakaoController.java
+++ src/main/java/egov/test/controller/KakaoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import egov.test.component.KakaoAuthComponent;
@@ -111,37 +115,92 @@ public class KakaoController {
             final String authorization = kakaoAuthComponent.generateAuthorization(timestamp, nonce, sign);
             
             // JSON 데이터 생성
-            JSONObject json = new JSONObject();
-            json.put("partnerOrderId", orderVO.getPartnerOrderId());
-            json.put("orderType", orderVO.getOrderType());
-            json.put("pickup", new JSONObject()
-                    .put("location", new JSONObject()
-                            .put("basicAddress", orderVO.getPickupBasicAddress())
-                            .put("detailAddress", orderVO.getPickupDetailAddress())
-                            .put("latitude", orderVO.getPickupLatitude())
-                            .put("longitude", orderVO.getPickupLongitude()))
-                    .put("contact", new JSONObject()
-                            .put("name", orderVO.getPickupContactName())
-                            .put("phone", orderVO.getPickupContactPhone())));
-            json.put("dropoff", new JSONObject()
-                    .put("location", new JSONObject()
-                            .put("basicAddress", orderVO.getDropoffBasicAddress())
-                            .put("detailAddress", orderVO.getDropoffDetailAddress())
-                            .put("latitude", orderVO.getDropoffLatitude())
-                            .put("longitude", orderVO.getDropoffLongitude()))
-                    .put("contact", new JSONObject()
-                            .put("name", orderVO.getDropoffContactName())
-                            .put("phone", orderVO.getDropoffContactPhone())));
-            json.put("productInfo", new JSONObject()
-                    .put("trayCount", orderVO.getTrayCount())
-                    .put("size", orderVO.getSize())
-                    .put("totalPrice", orderVO.getProductPrice() * orderVO.getProductQuantity())
-                    .put("products", new JSONArray()
-                    		.put(new JSONObject()
-	                            .put("name", orderVO.getProductName())
-	                            .put("quantity", orderVO.getProductQuantity())
-	                            .put("price", orderVO.getProductPrice())
-	                            .put("detail", orderVO.getProductDetail()))));
+//            JSONObject json = new JSONObject();
+//            json.put("partnerOrderId", orderVO.getPartnerOrderId());
+//            json.put("orderType", orderVO.getOrderType());
+//            json.put("pickup", new JSONObject()
+//                    .put("location", new JSONObject()
+//                            .put("basicAddress", orderVO.getPickupBasicAddress())
+//                            .put("detailAddress", orderVO.getPickupDetailAddress())
+//                            .put("latitude", orderVO.getPickupLatitude())
+//                            .put("longitude", orderVO.getPickupLongitude()))
+//                    .put("contact", new JSONObject()
+//                            .put("name", orderVO.getPickupContactName())
+//                            .put("phone", orderVO.getPickupContactPhone())));
+//            json.put("dropoff", new JSONObject()
+//                    .put("location", new JSONObject()
+//                            .put("basicAddress", orderVO.getDropoffBasicAddress())
+//                            .put("detailAddress", orderVO.getDropoffDetailAddress())
+//                            .put("latitude", orderVO.getDropoffLatitude())
+//                            .put("longitude", orderVO.getDropoffLongitude()))
+//                    .put("contact", new JSONObject()
+//                            .put("name", orderVO.getDropoffContactName())
+//                            .put("phone", orderVO.getDropoffContactPhone())));
+//            json.put("productInfo", new JSONObject()
+//                    .put("trayCount", orderVO.getTrayCount())
+//                    .put("size", orderVO.getSize())
+//                    .put("totalPrice", orderVO.getProductPrice() * orderVO.getProductQuantity())
+//                    .put("products", new JSONArray()
+//                    		.put(new JSONObject()
+//	                            .put("name", orderVO.getProductName())
+//	                            .put("quantity", orderVO.getProductQuantity())
+//	                            .put("price", orderVO.getProductPrice())
+//	                            .put("detail", orderVO.getProductDetail()))));
+            
+	        JSONObject json = new JSONObject();
+	
+	        // "pickup" 객체 생성
+	        JSONObject pickupLocation = new JSONObject();
+	        pickupLocation.put("basicAddress", orderVO.getPickupBasicAddress());
+	        pickupLocation.put("detailAddress", orderVO.getPickupDetailAddress());
+	        pickupLocation.put("latitude", orderVO.getPickupLatitude());
+	        pickupLocation.put("longitude", orderVO.getPickupLongitude());
+	
+	        JSONObject pickupContact = new JSONObject();
+	        pickupContact.put("name", orderVO.getPickupContactName());
+	        pickupContact.put("phone", orderVO.getPickupContactPhone());
+	
+	        JSONObject pickup = new JSONObject();
+	        pickup.put("location", pickupLocation);
+	        pickup.put("contact", pickupContact);
+	
+	        // "dropoff" 객체 생성
+	        JSONObject dropoffLocation = new JSONObject();
+	        dropoffLocation.put("basicAddress", orderVO.getDropoffBasicAddress());
+	        dropoffLocation.put("detailAddress", orderVO.getDropoffDetailAddress());
+	        dropoffLocation.put("latitude", orderVO.getDropoffLatitude());
+	        dropoffLocation.put("longitude", orderVO.getDropoffLongitude());
+	
+	        JSONObject dropoffContact = new JSONObject();
+	        dropoffContact.put("name", orderVO.getDropoffContactName());
+	        dropoffContact.put("phone", orderVO.getDropoffContactPhone());
+
+	        JSONObject dropoff = new JSONObject();
+	        dropoff.put("location", dropoffLocation);
+	        dropoff.put("contact", dropoffContact);
+	
+	        // "productInfo" 객체 생성
+	        JSONArray productsArray = new JSONArray();
+	        JSONObject product = new JSONObject();
+	        product.put("name", orderVO.getProductName());
+	        product.put("quantity", orderVO.getProductQuantity());
+	        product.put("price", orderVO.getProductPrice());
+	        product.put("detail", orderVO.getProductDetail());
+	
+	        productsArray.put(product);
+	
+	        JSONObject productInfo = new JSONObject();
+	        productInfo.put("trayCount", orderVO.getTrayCount());
+	        productInfo.put("size", orderVO.getSize());
+	        productInfo.put("totalPrice", orderVO.getProductPrice() * orderVO.getProductQuantity());
+	        productInfo.put("products", productsArray);
+	
+	        // 최종 JSON 객체에 데이터 삽입
+	        json.put("partnerOrderId", orderVO.getPartnerOrderId());
+	        json.put("orderType", orderVO.getOrderType());
+	        json.put("pickup", pickup);
+	        json.put("dropoff", dropoff);
+	        json.put("productInfo", productInfo);
             
             System.out.println("Generated JSON: " + json.toString());
             
@@ -152,7 +211,12 @@ public class KakaoController {
             headers.set("vendor", VENDOR_ID);
 
             // HTTP 요청 생성
+//            String testJsonString = "{\"partnerOrderId\":\"{연동사 주문 ID - 유니크 해야합니다}\",\"orderType\":\"QUICK\",\"pickup\":{\"location\":{\"latitude\":37.4354059,\"basicAddress\":\"서울특별시 강남구 역삼동 xxx\",\"detailAddress\":\"1층\",\"longitude\":126.74551},\"contact\":{\"name\":\"전달하는 사람 이름\",\"phone\":\"010-1000-0001\"}},\"dropoff\":{\"location\":{\"basicAddress\":\"서울특별시 강남구 일원동 xxx\",\"detailAddress\":\"2층\",\"latitude\":37.569691,\"longitude\":126.825791},\"contact\":{\"name\":\"받는 사람 이름\",\"phone\":\"010-1000-0002\"}},\"productInfo\":{\"trayCount\":1,\"size\":\"XS\",\"totalPrice\":29800,\"products\":[{\"name\":\"양념게장 (대)\",\"quantity\":\"1\",\"price\":29800,\"detail\":\"터지면 큰일남\"}]}}";
             HttpEntity<String> requestEntity = new HttpEntity<>(json.toString(), headers);
+//            HttpEntity<String> requestEntity = new HttpEntity<>(testJsonString, headers);
+            
+//            System.out.println("testJsonString : " + testJsonString);
+            System.out.println("Authorization : " + authorization);
             
             // API 호출
 //            ResponseEntity<String> response = restTemplate.postForEntity(API_ENDPOINT, requestEntity, String.class);
@@ -166,9 +230,31 @@ public class KakaoController {
                 System.out.println("HTTP Error Status Code: " + e.getStatusCode());
                 System.out.println("HTTP Error Response Body: " + e.getResponseBodyAsString());
                 e.printStackTrace();
+            } catch (HttpMessageNotReadableException e) {
+                System.out.println("JSON 형식 오류: " + e.getMessage());
+                e.printStackTrace();
+            } catch (RestClientException e) {
+            	if (e instanceof HttpClientErrorException) {
+                    HttpClientErrorException httpError = (HttpClientErrorException) e;
+                    System.out.println("HTTP 에러 발생: " + httpError.getStatusCode());
+                    System.out.println("응답 본문: " + httpError.getResponseBodyAsString());
+                } else if (e instanceof HttpServerErrorException) {
+                    HttpServerErrorException serverError = (HttpServerErrorException) e;
+                    System.out.println("서버 에러 발생: " + serverError.getStatusCode());
+                    System.out.println("응답 본문: " + serverError.getResponseBodyAsString());
+                } else if (e instanceof ResourceAccessException) {
+                    System.out.println("리소스 접근 에러: " + e.getMessage());
+                } else {
+                    System.out.println("기타 예외 발생: " + e.getMessage());
+                }
+                e.printStackTrace();
             } catch (Exception e) {
+                System.out.println("기타 예외 발생");
                 e.printStackTrace();
             }
+            
+            if (response == null) System.out.println("response is null");
+            else System.out.println("response is not null");
 
             // 응답 처리
             model.addAttribute("responseCode", response.getStatusCodeValue());
