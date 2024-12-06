20c1c89a463e25e3f99ba469003fed6687db627a
YoRangS <21900673@handong.ac.kr>
2024-12-06 19:48:21 +0900

    Security Check

--- src/main/java/egov/test/config/ApiConfigValue.java
+++ src/main/java/egov/test/config/ApiConfigValue.java
@@ -1,7 +1,7 @@
 package egov.test.config;
 
 public class ApiConfigValue {
-	public static String APIKeyValue = "42528957-8257-4dac-b9e5-32e3023e2bac";
+	public static String APIKeyValue = "";
 	
 	public static String venderID = "VZQS0G";
 	
